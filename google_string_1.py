7ec1b1b529b427759f040cd8340724a2e49a8637
gopalramasamy <53190708+gopalramasamy@users.noreply.github.com>
2019-08-05 21:40:58 +0530

    google_string_1_gopal
    google_string_1_gopal

--- google_string_1.py
+++ google_string_1.py
@@ -1,8 +1,13 @@
+
+
+
+'''
+
 def donuts(count):
   # +++your code here+++
 	#count=s
 	if count > 10:
-		a="donuts ({0}) returns Number of donuts: many".format(count)
+		a="donuts({0}) returns Number of donuts: many".format(count)
 	else:
 		a="donuts({0}) returns Number of donuts: {0}".format(count)
 	#print(a)
@@ -11,14 +16,52 @@ def donuts(count):
 	
 a=donuts(int(input()))
 print(a)
+'''
 
 '''
-def donuts(count):
-  # +++your code here+++
-  # LAB(begin solution)
-  if count < 10:
-    return 'Number of donuts: ' + str(count)
-  else:
-    return 'Number of donuts: many'
+print ('both_ends')
+
+def both_ends(s):
+	if len(s) > 2:
+		str1=s[0:2]
+		str2=s[-2:]
+		print(str1 + str2)
+		return str1 + str2
+	else:
+		return ''
 	
-	'''
+  # +++your code here+++
+	#return (str1+str2)
+  
+a=(both_ends('GOPALRAMASAMY'))
+print(a)
+'''
+
+'''
+	
+def fix_start(s):
+  # +++your code here+++
+	front = s[0]
+	back = s[1:]
+	fixed_back = back.replace(front, '*')
+	return front + fixed_back
+
+print(fix_start('GOPALGOPALGOPALGGGGGGGGGGGGgggg'))	
+
+'''
+'''
+def mix_up(a, b):
+  # +++your code here+++
+  str1=a
+  str2=b
+  
+  print( b + ' ' + a)
+  a_swapped = b[:2] + a[2:]
+  b_swapped = a[:2] + b[2:]
+  
+  c= a_swapped + ' ' + b_swapped 
+  
+  return c
+
+print(mix_up('GOPAL', 'RAM'))
+'''
